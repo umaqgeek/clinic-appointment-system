6ef0234e0945eb519e870366023704aae186d907
Umar Hambaran <omar.hambaran@frogasia.com>
2019-07-15 23:14:11 +0800

    feat: production auth db

--- conndb.php
+++ conndb.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "P@ssw0rd";
-$dbname = "cass_db";
+$servername = "us-cdbr-iron-east-02.cleardb.net";
+$username = "b84e6047168f42";
+$password = "b08b6b42";
+$dbname = "heroku_e6e43bd8f6bdf21";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
