a80172892311ae780db9ba1dbf4ac282f0ac7fbf
Umar Hambaran <omar.hambaran@frogasia.com>
2019-07-16 17:10:05 +0800

    feat: logo

--- header.php
+++ header.php
@@ -3,11 +3,15 @@
 <script src="<?=BASE_URL ?>/assets/js/jquery-3.4.1.min.js"></script>
 <script src="<?=BASE_URL ?>/assets/js/bootstrap.js"></script>
 
+<link rel="icon" href="<?=BASE_URL ?>/assets/images/icon01.png">
+
 <div class="container" style="padding-top: 2%;">
   <div class="row">
     <div class="col-md-12">
       <center>
-          <h2 onclick="window.location='<?=BASE_URL ?>/index.php'" style="cursor: pointer;">Clinic Appointment System</h2>
+          <h2 onclick="window.location='<?= BASE_URL ?>/index.php'" style="cursor: pointer;">Clinic Appointment System
+              <img src="<?= BASE_URL ?>/assets/images/icon01.png" style="max-width: 80px;" />
+          </h2>
       </center>
     </div>
   </div>
