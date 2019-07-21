daef40224587ed6f80a1accb6360a4e7d6c91e91
Umar Hambaran <omar.hambaran@frogasia.com>
2019-07-22 00:41:20 +0800

    fix: bugs

--- config/settings.php
+++ config/settings.php
@@ -1,7 +1,11 @@
 <?php
 
+$cloud_name = '';
+$api_key = '';
+$api_secret = '';
+
 \Cloudinary::config(array(
-    'cloud_name' => '' || $_ENV['CLOUDINARY_CLOUD_NAME'],
-    'api_key' => '' || $_ENV['CLOUDINARY_API_KEY'],
-    'api_secret' => '' || $_ENV['CLOUDINARY_API_SECRET']
+    'cloud_name' => $cloud_name == '' ? $_ENV['CLOUDINARY_CLOUD_NAME'] : $cloud_name,
+    'api_key' => $api_key == '' ? $_ENV['CLOUDINARY_API_KEY'] : $api_key,
+    'api_secret' => $api_secret == '' ? $_ENV['CLOUDINARY_API_SECRET'] : $api_secret
 ));
