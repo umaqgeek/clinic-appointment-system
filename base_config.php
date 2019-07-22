014ae833c039703145782ba52de71fc9eb57909c
Umar Hambaran <omar.hambaran@frogasia.com>
2019-07-22 11:26:19 +0800

    fix: fix auth

--- base_config.php
+++ base_config.php
@@ -3,17 +3,21 @@
 $base_dir = __DIR__;
 
 // server protocol
-$protocol = 'http';
+//$protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+$protocol = 'https';
 
 // domain name
 $domain = $_SERVER['SERVER_NAME'];
 
 // base url
+//$base_url = 'clinicAppointmentSystemSyera'; 
 $base_url = ''; 
+//$base_doc = preg_replace("!^${doc_root}!", '', $base_dir);
 $base_doc = $base_dir;
 
 // server port
-$port = '80';
+//$port = $_SERVER['SERVER_PORT'];
+$port = '443';
 $disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
 
 // put em all together to get the complete base URL
